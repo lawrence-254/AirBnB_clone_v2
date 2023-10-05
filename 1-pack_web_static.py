5373229986692c5c6f533ca1a88826e1f15b79d2
lawrence-254 <lodhiambo26@gmail.com>
2023-10-05 18:08:34 +0000

    1

---
+++ 1-pack_web_static.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""
+a Fabric script that generates a .tgz archive
+from the contents of the web_static folder
+"""
+import os
+from fabric.api import local
+from datetime import datetime
+
+
+def do_pack():
+	"""generates a .tgz archive from the contents of the web_static folder
+	"""
+	time = datetime.now().strftime("%Y%m%d%H%M%S")
+	os.makedirs("versions") if not os.path.exists("versions") else None
+
+	archive_name = "web_static_" + time + ".tgz"
+	archive = local("tar -czvf versions/{} web_static".format(archive_name))
+
+	return "version/" + archive_name if archive.succeeded else None
