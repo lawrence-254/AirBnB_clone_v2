ddf45ddb78036e9d6f146089045eb1f84c33104d
lawrence@254 <lodhiambo26@gmail.com>
2023-10-20 07:11:29 +0000

    corr

--- console.py
+++ console.py
@@ -115,14 +115,24 @@ class HBNBCommand(cmd.Cmd):
 
     def do_create(self, args):
         """ Create an object of any class"""
-        if not args:
+	list_of_args = args.split(" ")
+	class_name = list_of_args[0]
+	attributes = {}
+        if not class_name:
             print("** class name missing **")
             return
-        elif args not in HBNBCommand.classes:
+        elif class_name not in HBNBCommand.classes:
             print("** class doesn't exist **")
             return
-        new_instance = HBNBCommand.classes[args]()
-        storage.save()
+	if (len(list_of_args) > 1):
+		for argument in list_of_args[1:]:
+			k, v = argument.split("=")
+			attributes[k] = v.strip("\"'").replace("_", " ")
+
+        new_instance = HBNBCommand.classes[class_name]()
+        for k, v in attributes.items():
+		setattr(new_instance, k, v)
+	storage.save()
         print(new_instance.id)
         storage.save()
 
