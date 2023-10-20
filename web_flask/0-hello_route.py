8fadf51fd304ae214f34f9214199d3c3cd2132cf
lawrence254 <lodhiambo26@gmail.com>
2023-10-20 12:41:45 -0700

    two routes

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -1,6 +1,5 @@
 #!/usr/bin/python3
 '''A script that starts a Flask web application'''
-
 from flask import Flask
 
 app = Flask(__name__)
@@ -9,7 +8,7 @@ app = Flask(__name__)
 @app.route('/', strict_slashes=False)
 def hello():
     """Returns 'Hello HBNB!'"""
-    return 'Hello HBNB'
+    return 'Hello HBNB!'
 
 
 if __name__ == "__main__":
--- web_flask/1-hbnb_route.py
+++ web_flask/1-hbnb_route.py
@@ -1,21 +1,20 @@
 #!/usr/bin/python3
 '''a script that starts a Flask web application'''
-
-
 from flask import Flask
 
 app = Flask(__name__)
+
 
 @app.route('/', strict_slashes=False)
 def hello():
     """returns 'Hello HBNB!'"""
     return 'Hello HBNB!'
 
+
 @app.route("/hbnb", strict_slashes=False)
 def display_hbnb():
     """returns HBNB"""
     return "HBNB"
-
 
 
 if __name__ == "__main__":
